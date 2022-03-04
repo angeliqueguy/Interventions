6eb20d4a2f9c1e4478d44b7475624712b6e4f42b
angeliqueguy <angelique.guy12@gmail.com>
2022-03-04 12:18:50 -0500

    TP7-test4 prenom fonctionne

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -23,24 +23,32 @@ describe('ProblemeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-   });
+  });
 
-   it("#1 | Zone PRÉNOM invalide avec 2 caractèress", () =>{
+  it("#1 | Zone PRÉNOM invalide avec 2 caractèress", () =>{
     let zone = component.problemeForm.controls['prenom'];
     zone.setValue("a".repeat(2));
     let errors = zone.errors || {};
     expect(errors['minlength']).toBeTruthy();
-    });
+  });
 
-    it("#2 | Zone PRÉNOM valide avec 3 caractères", () =>{
-      let zone = component.problemeForm.controls['prenom'];
-      zone.setValue("a".repeat(3));
-      expect(zone.valid).toBeTruthy();
-      });
+  it("#2 | Zone PRÉNOM valide avec 3 caractères", () =>{
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue("a".repeat(3));
+    expect(zone.valid).toBeTruthy();
+  });
 
-      it("#3 | Zone PRÉNOM valide avec 200 caractères", () =>{
-        let zone = component.problemeForm.controls['prenom'];
-        zone.setValue("a".repeat(200));
-        expect(zone.valid).toBeTruthy();
-        });  
+  it("#3 | Zone PRÉNOM valide avec 200 caractères", () =>{
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue("a".repeat(200));
+    expect(zone.valid).toBeTruthy();
+  }); 
+  
+  it("#4 | Zone PRÉNOM invalide avec aucune valeur", () =>{
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue('');
+    let errors = zone.errors || {};
+    expect(errors['required']).toBeTruthy();
+  }); 
+  
 });
