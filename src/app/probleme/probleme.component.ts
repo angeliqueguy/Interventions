fffed2fc94d88a4ccb5ef08b395d322c5e65dea0
angeliqueguy <angelique.guy12@gmail.com>
2022-03-16 14:02:46 -0400

    TP10

--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { VerifierCaracteresValidator } from '../shared/longueur-minimum/longueur-minimum.component';
+import { ITypeProbleme } from './typesprobleme';
+import { TypesproblemeService } from './typesprobleme.service';
 
 @Component({
   selector: 'Inter-probleme',
@@ -8,17 +10,39 @@ import { VerifierCaracteresValidator } from '../shared/longueur-minimum/longueur
   styleUrls: ['./probleme.component.css']
 })
 export class ProblemeComponent implements OnInit {
-  problemeForm: FormGroup;
+problemeForm: FormGroup;
+typesProb: ITypeProbleme[];
+errorMessage: string;
+//probleme: IProbleme;
+messageSauvegarde: string;
 
-  constructor(private fb: FormBuilder) { }
+constructor(private fb: FormBuilder, private typeproblemeService: TypesproblemeService) { }
 
-  ngOnInit(): void {
-    this.problemeForm = this.fb.group({
-     prenom: ['' , [VerifierCaracteresValidator.longueurMinimum(3), Validators.required]],
-    })
+
+
+ngOnInit(): void {
+  this.problemeForm = this.fb.group({
+    prenom: ['' , [Validators.required, VerifierCaracteresValidator.longueurMinimum(3)]],
+    nom: ['', [Validators.required, Validators.maxLength(50)]],
+    noTypeProbleme: ['', Validators.required],
+    courrielGroup: this.fb.group({
+    courriel: [{value: '', disabled: true}],
+    courrielConfirmation: [{value: '', disabled: true}],
+    }),
+    telephone: [{value: '', disabled: true}]
+    });
+this.typeproblemeService.obtenirTypesProbleme()
+.subscribe(typesP => this.typesProb = typesP,
+error => this.errorMessage = <any>error);
+}
+
+
+
+save(): void {
   }
 
-  save(): void {
-  }
+
+  
+ 
 
 }
